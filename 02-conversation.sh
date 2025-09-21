74220e15209fac5975c41275e3c93fd265264863
Hareesh Reddy <hareesh.iamreddy@gmail.com>
2025-09-21 11:29:37 +0530

    02-conversation.sh

---
+++ 02-conversation.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+echo "ramesh:: Hi suresh,How are you?"
+echo "suresh:: Hello ramesh,I am fine, How are you ?"
+echo "ramesh:: what are you learning recently ?"
+echo "suresh:: I am learning shell scripting ramesh" 
