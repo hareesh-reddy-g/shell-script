a20bc4baa2fc02b67edb0ae646ff1be9288accfe
Hareesh Reddy <hareesh.iamreddy@gmail.com>
2025-09-21 12:06:08 +0530

    sdhsgd

---
+++ 03-variables.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+person1=john
+person2=raheem
+echo"$person1:: Hi $person2 ,How are you?"
+echo"$person2:: Hello $person1,Iam fine ,what about you?"
+echo$person1:: what are you learning recently?"
+echo"$person2::I am learning shell scripting $person1"
