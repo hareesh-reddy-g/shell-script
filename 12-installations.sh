7b551c2a21d7a384aec48db5f045ba90c9a35f19
Hareesh Reddy <hareesh.iamreddy@gmail.com>
2025-09-23 17:16:26 +0530

    MYSQL installation

---
+++ 12-installations.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+USERID=$(id -u)
+
+if [ $? -ne 0 ]; then
+    echo "ERROR:: Please run this script with root privelege"
+    exit 1
+
+if [ $? -ne 0 ]; then
+    echo "ERROR::Installing MYSQL is failure"
+    exit 1
+else
+    echo "Installing MYSQL is SUCCESS"
+fi
