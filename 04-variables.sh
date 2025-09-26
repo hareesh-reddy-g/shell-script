7eb251855f34600f09e7222aef29d3a82e88db30
Hareesh Reddy <hareesh.iamreddy@gmail.com>
2025-09-26 18:37:15 +0530

    variable name special symbels

--- 04-variables.sh
+++ 04-variables.sh
@@ -3,7 +3,7 @@
 PERSON1=$1
 PERSON2=$2
 
-echo "$PERSON1:: welcome to Alaska $PERSON2"
+echo "$PERSON1:: Welcome to Alaska $PERSON2"
 echo "$PERSON2:: Thank you $PERSON1, Finally we are not on zoom webinar"
 echo "$PERSON1:: So, do we have a deal"
 echo "$PERSON2:: yes, if you make Ukraine joins Russsia"
