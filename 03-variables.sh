c718bd9a7d54d0ae21abbc2c304853a2bcc70c38
Hareesh Reddy <hareesh.iamreddy@gmail.com>
2025-09-26 13:44:36 +0530

    variables

---
+++ 03-variables.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+PERSON1=John
+PERSON2=Raheem
+echo "$PERSON1:: Hi $PERSON2 How are you ?"
+echo "$PERSON2:: Hello $PERSON2 I am good What about you ?"
+echo "$PERSON1:: what are you learning recently ?"
+echo "$PERSON2:: I am learning DevSecOps with AWS $PERSON1"
