76c07b9e1975551de51c045b777794bda905fc81
Hareesh Reddy <hareesh.iamreddy@gmail.com>
2025-09-28 09:16:26 +0530

    colors

--- 14-colors.sh
+++ 14-colors.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-R=\e"[31m"
-G=\e"[32m"
-Y=\e"[33m"
-B=\e"[34m"
-N=\e"[0m"
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+B="\e[34m"
+N="\e[0m"
 
-echo -e " $R Hello world $N"
+echo -e "$R Hello world $N"
 echo "check this color"
