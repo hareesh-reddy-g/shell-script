9b670e847e54de471987d0fd8840b62e2c418715
Hareesh Reddy <hareesh.iamreddy@gmail.com>
2025-09-28 08:22:38 +0530

    3 package installation

--- 13-3package-installation.sh
+++ 13-3package-installation.sh
@@ -1,4 +1,26 @@
 #!/bin/bash
 
-dhsgdhsbdhsgd
-dsgdxhsbxnsx
+USERID=$(id -u)
+
+if [ $USERID -ne 0 ]; then
+    echo "ERROR:: please run this script with root privelege"
+    exit 1
+fi
+ 
+VALIDATE(){
+    if [ $1 -ne 0 ]; then
+        echo "ERROR:: Installing $2 is failure"
+        exit 1
+    else
+        echo "Installing $2 is SUCCESS"
+    fi
+}
+
+dnf install mysql -y
+VALIDATE $? "MYSQL"
+
+dnf install nginx -y
+VALIDATE $? "nginx"
+
+dnf install mongodb-mongosh -y
+VALIDATE $? "mongosh"
